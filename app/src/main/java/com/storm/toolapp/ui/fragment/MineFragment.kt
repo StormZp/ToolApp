4e0e9d6df46bcde8da6f4402b1b10647b6685925
zzpStorm <409348950@qq.com>
2017-12-14 23:48:49 +0800

    [add]  dialog

--- app/src/main/java/com/storm/toolapp/ui/fragment/MineFragment.kt
+++ app/src/main/java/com/storm/toolapp/ui/fragment/MineFragment.kt
@@ -1,14 +1,15 @@
 package com.storm.toolapp.ui.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.storm.tool.utils.AppUtil
+import com.storm.tool.view.MyPopupWindow
 import com.storm.toolapp.R
 import com.storm.toolapp.base.BaseFragment
 import com.storm.toolapp.databinding.FragmentMineBinding
-import com.storm.toolapp.dialog.MyBlankDialog
 
 /**
  * Created Storm
@@ -17,6 +18,9 @@ import com.storm.toolapp.dialog.MyBlankDialog
  */
 
 class MineFragment : BaseFragment<FragmentMineBinding>() {
+    private var list = arrayListOf<String>()
+    private var menu: MyPopupWindow? = null
+
     companion object {
         private var ARG_PARAM1: String? = "param1"
         private var ARG_PARAM2: String? = "param2"
@@ -52,11 +56,27 @@ class MineFragment : BaseFragment<FragmentMineBinding>() {
     override fun initData() {
         binding.titlebar.title.text = mParam1
         binding.titlebar.back.visibility = View.GONE
+
+
+        for (i in 0..20) {
+            list.add("条目:" + i)
+        }
+        menu = MyPopupWindow(context, list, object : MyPopupWindow.OnMenuItemClickListener {
+            override fun onItemClick(position: Int, src: String) {
+                Toast.makeText(context, position.toString() + "----" + src, Toast.LENGTH_SHORT).show()
+            }
+        })
+        menu!!.setOnDismissListener { Toast.makeText(context, "menu消失了", Toast.LENGTH_SHORT).show() }
+
+        menu!!.height = AppUtil.getScreenHeight(activity) - AppUtil.dip2Px(activity, 88F) - AppUtil.getNavigationBarHeight(activity) + AppUtil.getStatusBarHeight(activity) - 1
+
     }
 
     override fun initListener() {
         binding.submit.setOnClickListener {
-            activity.startActivity(Intent(activity, MyBlankDialog::class.java))
+            //            activity.startActivity(Intent(activity, MyBlankDialog::class.java))
+            menu!!.showAsDropDown(binding.submit, 0, 10)
+
         }
     }
 
---
+++ tool/src/main/java/com/storm/tool/view/MyPopupWindow.kt
@@ -0,0 +1,93 @@
+package com.storm.tool.view
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.*
+import com.meidp.tool.R
+import com.storm.tool.utils.AppUtil
+
+/**
+ * @创建作者 Storm
+ * @创建时间 2017-12-14 23:22
+ * @创建描述 ${控件下面的弹框}
+ */
+
+class MyPopupWindow(context: Context, arrayList: ArrayList<String>,listener: OnMenuItemClickListener ) : PopupWindow() {
+    private val HEIGHT_MENU = 160
+
+    var context = context
+    var list = arrayList
+    var listener = listener
+
+    init {
+        initView()
+    }
+
+    private fun initView() {
+        val scroll = ScrollView(context)
+        scroll.setBackgroundColor(Color.WHITE)
+        val group = RadioGroup(context)
+
+        for (i in list.indices) {
+            val sort = list[i]
+            val rb = RadioButton(context)
+            rb.setText(sort)
+            rb.setTextColor(Color.parseColor("#404040"))
+            rb.buttonDrawable = ColorDrawable(Color.TRANSPARENT)
+            rb.setBackgroundResource(R.drawable.abc_ab_share_pack_mtrl_alpha)
+            rb.gravity = Gravity.CENTER_VERTICAL
+            rb.setPadding(AppUtil.dip2Px(context, 10F), 0, 0, 0)
+            rb.setOnCheckedChangeListener { button, isChecked ->
+                if (isChecked) {
+                    button.setTextColor(Color.parseColor("#0FAEAA"))
+                    if (null != listener) {
+                        listener.onItemClick(i, list[i])
+                        dismiss()
+                    }
+                } else {
+                    button.setTextColor(Color.parseColor("#404040"))
+
+                }
+            }
+
+            val layoutParams = RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, AppUtil.dip2Px(context, 35F))
+            group.addView(rb, layoutParams)
+
+            if (0 == i) {
+                rb.isChecked = true
+            }
+        }
+        scroll.addView(group)
+
+        val layout = LinearLayout(context)
+        layout.orientation = LinearLayout.VERTICAL
+        layout.addView(scroll, LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, AppUtil.dip2Px(context, HEIGHT_MENU.toFloat())))
+
+        val frameLayout = FrameLayout(context)
+        frameLayout.setBackgroundColor(Color.parseColor("#99666666"))
+        frameLayout.setOnTouchListener { view, motionEvent ->
+            dismiss()
+            false
+        }
+
+        layout.addView(frameLayout, LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+
+        contentView = layout
+
+        //setHeight(AppUtil.getScreenHeight((Activity) context) - AppUtil.dip2Px(context, 88) - AppUtil.getNavigationBarHeight(context) + AppUtil.getStatusBarHeight(context));
+        width = ViewGroup.LayoutParams.MATCH_PARENT
+        isFocusable = true
+        isOutsideTouchable = true
+        setBackgroundDrawable(BitmapDrawable())
+        animationStyle = R.style.popmenu_anim_style
+    }
+
+
+    open interface OnMenuItemClickListener {
+        fun onItemClick(position: Int, src: String)
+    }
+}
